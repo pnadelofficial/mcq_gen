f09cb60a569ed4b3e30b890afd13b8102015f26f
peter nadel <peter.m.nadel@gmail.com>
2024-08-26 11:36:29 -0400

    data dir for new folders per name

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ ex_uploaded_files = st.file_uploader(
 )
 
 for uploaded_file in cc_uploaded_files:
-    subprocess.run(['ls'])
+    subprocess.run(['ls -la'])
     with open(f"./data/{name}/cc_{uploaded_file.name}", "wb") as f:
         f.write(uploaded_file.getbuffer())
 
